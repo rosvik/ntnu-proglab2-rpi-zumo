bd7a335f2b515be586b39482404de25b4edb54e6
Pål Fossnes <paul.fossnes@gmail.com>
2017-11-07 16:08:52 +0100

    Firstdraft motob

--- Motob.py
+++ Motob.py
@@ -1,15 +1,18 @@
-class Motob:
 
+
+class Motob:
 
     def __init__(self):
         self.motors = []
         self.value = None
 
+    #receive a new motor recommendation, load it into the value slot, and operationalize it
     def update(self):
         return
 
+    #convert a motor recommendation into one or more motor settings, which are sent to the corresponding motor(s)
     def operationalize(self):
         return
 
-    
+
 
