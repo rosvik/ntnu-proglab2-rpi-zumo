f9770af80e793e5e246b3ac7887f3a967836821f
Pål Fossnes <paul.fossnes@gmail.com>
2017-11-07 16:05:59 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Motob.py
@@ -0,0 +1,15 @@
+class Motob:
+
+
+    def __init__(self):
+        self.motors = []
+        self.value = None
+
+    def update(self):
+        return
+
+    def operationalize(self):
+        return
+
+    
+
