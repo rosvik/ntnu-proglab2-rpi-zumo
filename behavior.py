10e9976bfa2b01bb70f2f6b564f4609698201fa4
Robot <j.rosvik@gmail.com>
2017-11-17 10:48:52 +0100

    Fixed speed on ultrasonic behavior

--- behavior.py
+++ behavior.py
@@ -57,7 +57,7 @@ class UV_behavior(Behavior):
         dist = self.sensobs[0].value
         if dist >= 10:
             self.bbcon.camera_activate = False
-            self.motor_rec = [('F', 0.25, 0.6)]
+            self.motor_rec = [('F', 0.4, 0.6)]
             self.match_degree = 0.6
             self.weight_update()
         else:
